1ed5666b4908426d6f724d914f0a0909fb170d2e
Lucas-AVS <lucas.alencar31@gmail.com>
2025-03-29 16:47:13 -0300

    feat: add enemy cpu board with random ship placement

--- src/functions/enemyBoard.js
+++ src/functions/enemyBoard.js
@@ -4,5 +4,38 @@ import renderBoard from "./renderBoard";
 export default function enemyBoard() {
   const enemyPlayer = new Player(false);
 
+  function deployRandomShips(player) {
+    if (player.gameBoard) {
+      function randomCoordinate() {
+        let y = Math.floor(Math.random() * 10);
+        let yPositionToLetter = (Number(y) + 9).toString(36); // Convert number to letter
+        let x = Math.floor(Math.random() * 10);
+        let orientation = Math.random() < 0.5;
+        return [yPositionToLetter, x, orientation];
+      }
+      function deployShipType(shipLength, quantity) {
+        for (let i = 0; i < quantity; i++) {
+          let [y, x, orientation] = randomCoordinate();
+          let result = player.gameBoard.placeShip(
+            shipLength,
+            [y, x],
+            orientation
+          );
+          if (
+            result === "invalid position" ||
+            result === "ship does not fit this coordinate" ||
+            result === "there is already a ship in this area"
+          ) {
+            i--;
+          }
+        }
+      }
+      deployShipType(4, 1); // destroyer
+      deployShipType(3, 2); // warship
+      deployShipType(2, 3); // submarine
+      deployShipType(1, 4); // boat
+    }
+  }
+  deployRandomShips(enemyPlayer);
   renderBoard(enemyPlayer, "enemy");
 }
